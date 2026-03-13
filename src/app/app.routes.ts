a235fde9d3c46b545c8e2ee1b56e97e6d7b05061
carlosalvahan <carlosalvahan@users.noreply.github.com>
2026-03-13 09:33:42 +0000

    deploy: 74a0a38198d394c4336ad038deeedcf876ab9697

--- src/app/app.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Routes } from '@angular/router';
-import { IntroductionComponent } from './pages/introduction/introduction.component';
-
-export const routes: Routes = [
-    { path: '', component: IntroductionComponent }, // Default route
-];
