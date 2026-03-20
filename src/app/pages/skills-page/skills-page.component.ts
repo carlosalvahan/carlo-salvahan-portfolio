b3634bcf69ebc9950ef91beeb20dbe011a0b580e
Carlo Salvahan <carlo.salvahan@gmail.com>
2026-03-20 17:54:54 +0800

    Initial push for Skills

--- src/app/pages/skills-page/skills-page.component.ts
+++ src/app/pages/skills-page/skills-page.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SkillsConstants } from '../../constants/skills.constants';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-skills-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills-page.component.html',
   styleUrl: './skills-page.component.scss'
 })
-export class SkillsPageComponent {
+export class SkillsPageComponent implements OnInit {
+  readonly skillSet = Object.keys(SkillsConstants).map(skill => {
+    return {
+      ...SkillsConstants[skill as keyof typeof SkillsConstants], name: skill
+    }
+  });
+  
 
+  ngOnInit(): void {
+    console.log(this.skillSet);
+  }
 }
