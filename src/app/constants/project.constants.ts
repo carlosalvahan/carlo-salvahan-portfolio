bbad5a9175963c55eda2d8643101f297289808b0
Carlo Salvahan <carlo.salvahan@gmail.com>
2026-03-31 02:54:34 +0800

    update projects page

--- src/app/constants/project.constants.ts
+++ src/app/constants/project.constants.ts
@@ -1,9 +1,105 @@
 const ProjectConstants = {
+    'U.S Veterans Insurance': {
+        company: 'IBM Solutions Inc.',
+        position: 'Senior Software Developer',
+        skills: ['ReactJS', 'JavaScript', 'HTML5', 'CSS3'],
+        details: 'handle property tax administration, including bill processing, asset tracking, and managing appeal schedules.',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-hand-holding-heart'
+    },
+    'Property Tax Management': {
+        company: 'IBM Solutions Inc.',
+        position: 'Senior Software Developer',
+        skills: ['Angular', 'JavaScript', 'HTML5', 'CSS3', 'Bootstrap'],
+        details: 'handle property tax administration, including bill processing, asset tracking, and managing appeal schedules.',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-building-columns'
+    },
+    'Tax Advisory': {
+        company: 'IBM Solutions Inc.',
+        position: 'Mid Software Developer',
+        skills: ['Angular', 'JavaScript', 'HTML5', 'CSS3', 'Bootstrap'],
+        details: 'provides users with guidance on tax deadlines and regulatory updates.',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-file-invoice-dollar'
+    },
+    'Insurance Suggestion Application': {
+        company: 'Prosource Global',
+        position: 'Mid Software Developer',
+        skills: ['Angular', 'JavaScript', 'HTML5', 'CSS3'],
+        details: 'designed to suggest suitable insurance plans based on user needs and preferences.',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-car-burst'
+    },
+    'Airline E-commerce': {
+        company: 'Indra Philippines',
+        position: 'Mid Software Developer',
+        skills: ['Angular', 'JavaScript', 'HTML5', 'CSS3', 'Bootstrap', 'Materialize'],
+        details: 'eCommerce platform for airline services and products.',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-cart-shopping'
+    },
+    'Airline Callcenter Application': {
+        company: 'Indra Philippines',
+        position: 'Mid Software Developer',
+        skills: ['Angular', 'JavaScript', 'HTML5', 'CSS3', 'Bootstrap', 'Materialize'],
+        details: 'designed for call center agents to handle flight reservations, promotional offers, and customer rewards efficiently.',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-phone-volume'
+    },
+    'Airline Flight Booking': {
+        company: 'Indra Philippines',
+        position: 'Mid Software Developer',
+        skills: ['Angular', 'JavaScript', 'HTML5', 'CSS3', 'Bootstrap', 'Materialize'],
+        details: 'facilitate seamless flight booking and reservation management.',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-plane-up'
+    },
+    'Smart ID Migration System': {
+        company: 'PCCW Solutions',
+        position: 'Junior Software Developer',
+        skills: ['Angular', 'JavaScript', 'HTML5', 'CSS3', 'Bootstrap', 'VueJS' ],
+        details: 'managing and executing the migration of smart identification card data and infrastructure.',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-id-card'
+    },
     'Timesheet System': {
         company: 'PCCW Solutions',
-        position: 'Software Developer',
-        skills: ['Java', 'MySQL', 'HTML5', 'CSS3', 'JavaScript', 'Bootstrap'],
-        details: '',
+        position: 'Junior Software Developer',
+        skills: [ 'JavaScript', 'HTML5', 'CSS3', 'Bootstrap', 'MySQL', 'Java' ],
+        details: 'designed to streamline employee time tracking and project-based charge assignments.',
         keyRoles: [
             'Develop and maintain pages',
             'Fix bugs and defects',
--- src/app/pages/projects-page/projects-page.component.ts
+++ src/app/pages/projects-page/projects-page.component.ts
@@ -1,11 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProjectConstants } from '../../constants/project.constants';
+import { SkillsConstants } from '../../constants/skills.constants';
 
 @Component({
   selector: 'app-projects-page',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects-page.component.html',
   styleUrl: './projects-page.component.scss'
 })
 export class ProjectsPageComponent {
+  readonly projectList = Object.keys(ProjectConstants).map(project => {
+    return {
+      ...ProjectConstants[project as keyof typeof ProjectConstants], name: project
+    }
+  }).map(projectWithSkill => {
+    return {
+      ...projectWithSkill, 
+      projectSkillDetail: projectWithSkill.skills.slice(0, 4).map(skill => {
+        return {
+          ...SkillsConstants[skill as keyof typeof SkillsConstants], name: skill
+        }
+      }),
+      skillDiff: projectWithSkill.skills.length - 4
+    }
+  });
+
+
+  constructor() {
+    console.log(this.projectList);
+  }
 
 }
