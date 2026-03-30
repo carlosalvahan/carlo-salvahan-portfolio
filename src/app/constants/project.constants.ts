4428b38d090bb1c386ca51ad5a853e9fbf0037d5
Carlo Salvahan <carlo.salvahan@gmail.com>
2026-03-30 21:25:19 +0800

    add content to project

---
+++ src/app/constants/project.constants.ts
@@ -0,0 +1,16 @@
+const ProjectConstants = {
+    'Timesheet System': {
+        company: 'PCCW Solutions',
+        position: 'Software Developer',
+        skills: ['Java', 'MySQL', 'HTML5', 'CSS3', 'JavaScript', 'Bootstrap'],
+        details: '',
+        keyRoles: [
+            'Develop and maintain pages',
+            'Fix bugs and defects',
+            'Maintain and Edit database'
+        ],
+        icon: 'fa-solid fa-clock'
+    }
+}
+
+export {ProjectConstants};
