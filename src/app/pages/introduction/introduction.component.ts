748cfc1883d11d372eed9044521d908a57bed10d
Carlo Salvahan <carlo.salvahan@gmail.com>
2026-04-22 17:11:13 +0800

    Add contact me and modal in intro

--- src/app/pages/introduction/introduction.component.ts
+++ src/app/pages/introduction/introduction.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { ScrollAnimateDirective } from '../../directives/scroll-animate.directive';
+import { ModalComponent } from '../../shared/modal/modal.component';
+import { ProjectDetailsComponent } from '../projects-page/project-details/project-details.component';
+import { ProjectConstants } from '../../constants/project.constants';
+import { CompaniesConstants } from '../../constants/experience.constants';
 
 @Component({
   selector: 'app-introduction',
-  imports: [ScrollAnimateDirective],
+  imports: [ScrollAnimateDirective, ModalComponent, ProjectDetailsComponent],
   templateUrl: './introduction.component.html',
   styleUrl: './introduction.component.scss'
 })
 export class IntroductionComponent {
+  showModal: boolean = false;
+  selectedProject: any;
 
+  modalClose() {
+    this.showModal = false;
+  }
+
+  openProject(projectName: string) {
+    const projectItem = ProjectConstants[projectName as keyof typeof ProjectConstants];
+    this.selectedProject = {
+      ...projectItem,
+      name: projectName,
+      cmpImage: CompaniesConstants[projectItem.company as keyof typeof CompaniesConstants]?.imageUrl
+    }
+    this.showModal = true;
+  }
 }
